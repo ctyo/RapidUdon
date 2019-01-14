785748f27eba0e7153c2a2bd90d1d0f00ad0215b
ctyo <ctyo@live.jp>
2019-01-14 14:04:12 +0900

    ポップアップ追加, うどん屋全件取得

--- src/ydf_to_geojson.py
+++ src/ydf_to_geojson.py
@@ -4,16 +4,23 @@ import json
 from geojson import Feature, Point, FeatureCollection
 import pprint
 
-appid = ''
-url = 'https://map.yahooapis.jp/search/local/V1/localSearch?appid='+appid+'&ac=37&query=%E3%81%86%E3%81%A9%E3%82%93&output=json&results=100'
-ydf = json.load(urllib.urlopen(url))
+def append_feature (ydf):    
+    for feature in ydf['Feature']:
+        ll = feature['Geometry']['Coordinates'].split(',')
+        point = Point((float(ll[0]), float(ll[1])))
+        link = 'https://loco.yahoo.co.jp/place/g-' + feature['Gid'] + '/'
+        feature_list.append(Feature(geometry=point, properties={'title' : feature['Name'], 'link' : link}))
 
 feature_list = []
-for feature in ydf['Feature']:
-    ll = feature['Geometry']['Coordinates'].split(',')
-    point = Point((float(ll[0]), float(ll[1])))
-    link = 'https://loco.yahoo.co.jp/place/' + feature['Gid'] + '/'
-    feature_list.append(Feature(geometry=point, properties={'title' : feature['Name'], 'link' : link}))
+appid = ''
+url = 'https://map.yahooapis.jp/search/local/V1/localSearch?appid='+appid+'&ac=37&gc=0101018&output=json&results=100&start='
+
+ydf = json.load(urllib.urlopen(url))
+append_feature(ydf)
+
+refetch_num = ydf['ResultInfo']['Total'] / 100
+for i in range(refetch_num):
+    start = 100*(i+1)+1
+    append_feature(json.load(urllib.urlopen(url+str(start))))
 
 print FeatureCollection(feature_list)
-
